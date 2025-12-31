3d3e68701d25258954297868f80addecdb00807a
joao-vitorb <joaovborges2005@hotmail.com>
2025-12-31 15:39:46 -0300

    chroe: remove unnecessary archives

--- src/shared/utils/id.ts
+++
@@ -1,5 +0,0 @@
-export function createId(prefix = "u"): string {
-  const random = Math.random().toString(36).slice(2, 8);
-  const time = Date.now().toString(36);
-  return `${prefix}_${time}_${random}`;
-}
